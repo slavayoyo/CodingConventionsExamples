d0267f5c067afed1db8a212136309903553296fc
Slava <slavavipx38@gmail.com>
2023-02-11 12:27:17 +0200

    First commit

---
+++ Quarterly tax calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace QuarterlyTaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\t >> Quarterly Tax Calculator << \n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            ulong minutesQuarter;
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter the number of minutes of calls for January: ");
+                    string enteredMinutesJanuary = Console.ReadLine();
+
+                    Console.WriteLine("Enter the number of minutes of calls for January: ");
+                    string enteredMinutesFabruary = Console.ReadLine();
+
+                    Console.WriteLine("Enter the number of minutes of calls for January: ");
+                    string enteredMinutesMarch = Console.ReadLine();
+
+                    if ((ulong.TryParse(enteredMinutesJanuary, out ulong minitesJanuary)) &&
+                        (ulong.TryParse(enteredMinutesFabruary, out ulong minitesFabruary)) &&
+                        (ulong.TryParse(enteredMinutesMarch, out ulong minitesMarch)))
+                    {
+                        checked
+                        {
+                            minutesQuarter = minitesJanuary + minitesFabruary + minitesMarch;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n [ERROR]: Invalid input. \n");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+            }
+
+            decimal totalCost; // UAN.
+            {
+                const decimal PRICE = 2; // uan.
+                totalCost = minutesQuarter * PRICE;
+            }
+
+            decimal tax; // UAN.
+            {
+                const decimal TAX_RATE = 20; // % 
+                tax = totalCost / 100 * TAX_RATE;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(); // Іndentation.
+            Console.WriteLine($"Total           : {minutesQuarter:N} minutes");
+            Console.WriteLine($"Сost of minutes : {totalCost:N} UAN");
+            Console.WriteLine($"Tax payable:    : {tax:N} UAN");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
